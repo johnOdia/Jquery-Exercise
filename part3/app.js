330d473bdf3eb5de343c4d1f44b93cf28017c25f
johnOdia <efosajohnodia@gmail.com>
2020-09-07 06:27:36 -1100

    final commit

--- part3/app.js
+++ part3/app.js
@@ -1,5 +1,9 @@
 $(function () {
 
+    //GLOBAL VARIABLES
+    let myMovies =  JSON.parse(localStorage.getItem('movies')) //All movie titles and ratings go in here as nested objects with the name as key which is saved to local storage.
+    const getMovies = () => myMovies =  JSON.parse(localStorage.getItem('movies'))
+
     //EVENT LISTENERS =>
     $('form').on('submit', handleSubmitForm)
     $('tbody').on('click', handleClickDelete)
@@ -7,8 +11,6 @@ $(function () {
 
 
     //EVENT HANDLERS =>
-    let moviesObject = {} //All movie titles and ratings go in here as nested objects with the name as key
-
     //Recieves input values and calls addMovie() helper function to render to the DOM
     function handleSubmitForm(e) {
         e.preventDefault();
@@ -16,9 +18,10 @@ $(function () {
         let rating = $('#rating').val()
         if (title !== '') {
             addMovie(title, rating)
-            moviesObject[title] = { name: title, rating: rating }
             $('#title').val('')
             $('#rating').val(0)
+            addToLocStorage(title,rating)
+            getMovies()
         }
     }
 
@@ -26,21 +29,26 @@ $(function () {
     function handleClickDelete(e) {
         let item = $(e.target).parent()
         if ($(e.target).text() === 'Delete') {
-            item.remove()
-            delete moviesObject[$(e.target).closest('tr').children('.col-8').text()]
+            item.on('transitionend', function(){
+                item.remove()   
+            })
+            item.addClass('movie-fall')
+            delete myMovies[$(e.target).closest('tr').children('.col-8').text()]
+            localStorage.setItem('movies',JSON.stringify(myMovies))
         }
     }
 
     //Makes use of 6 helper functions to sort movies by the parameters passed, one of the helpers calls addMovie() which renders the sorted movies to the DOM
+   
     function sortMovies(e) {
         let movieTitles = []
         let movieRatings = []
         let sortedTitles
         let sortedRatings
         $('tbody').text('')
-        for (let movie in moviesObject) {
-            movieTitles.push(moviesObject[movie].name)
-            movieRatings.push(moviesObject[movie].rating)
+        for (let movie in myMovies) {
+            movieTitles.push(myMovies[movie].name)
+            movieRatings.push(myMovies[movie].rating)
         }
         if ($(e.target).data('item') === 'sortNameUp') {
             sortedTitles = sortNameUp(movieTitles)
@@ -79,6 +87,7 @@ $(function () {
     <td class="col-2" scope='col'>${rating}</td>
     <td class="col-2 btn btn-danger m-1" scope='col'>Delete</td>
     `)
+    newMovie.addClass('movie')
         $('tbody').append(newMovie)
     }
 
@@ -116,9 +125,9 @@ $(function () {
     //This function accepts the earlier sorted ratings array as an argument and calls addMovie to render to the DOM
     function sortByRatings(ratingsArray) {
         return ratingsArray.map(val => {
-            for (let key in moviesObject) {
-                if (moviesObject[key].rating === val) {
-                    addMovie(moviesObject[key].name, moviesObject[key].rating)
+            for (let key in myMovies) {
+                if (myMovies[key].rating === val) {
+                    addMovie(myMovies[key].name, myMovies[key].rating)
                 }
             }
         })
@@ -127,14 +136,32 @@ $(function () {
     //Does same for the title
     function sortByTitle(titlesArray) {
         return titlesArray.map(val => {
-            addMovie(moviesObject[val].name, moviesObject[val].rating)
+            addMovie(myMovies[val].name, myMovies[val].rating)
         })
     }
+    
 
+    //tooltips
     $('[data-toggle="sortNameUp"]').tooltip()
     $('[data-toggle="sortNameDown"]').tooltip()
     $('[data-toggle="sortRatingUp"]').tooltip()
     $('[data-toggle="sortRatingDown"]').tooltip()
     $('[data-toggle="link"]').tooltip()
+
+    //local storage
+    let movieStorage = {}
+    function addToLocStorage(title,rating){
+        let storedMovies = JSON.parse(localStorage.getItem('movies'))
+        storedMovies[title] = { name: title, rating: rating }
+        moviesObject = storedMovies
+        localStorage.setItem('movies',JSON.stringify(moviesObject))
+    }
+
+    let renderStoredMovies = (function(){
+        let currentMovies = JSON.parse(localStorage.getItem('movies'))
+        for (movie in currentMovies){
+            addMovie(currentMovies[movie].name,currentMovies[movie].rating)
+        }
+    })()
 
 })
